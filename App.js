e1823ec71ae3d4883ec253b0cf952cb06a6519dd
Jugal Jain <jainjugal7@gmail.com>
2021-09-20 21:06:43 +0530

    Project started

---
+++ App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,ScrollView, Image, TextInput, Container } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}> 
+      <Text>
+        Hello CHANGE
+      </Text>
+    </View>
+  )
+  }
